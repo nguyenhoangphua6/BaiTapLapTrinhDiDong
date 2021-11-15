cc46abc04bee913aae2456142292f67e7619d040
Hoang Phu <nguyenhoangphua6@gmail.com>
2021-11-16 03:10:40 +0700

    da them phan design cua ket qua sau tim kiem (chua xong cai lich su di chuyen)

---
+++ app/src/main/java/com/midterm/lamnhom/KQTimKiem.java
@@ -0,0 +1,14 @@
+package com.midterm.lamnhom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KQTimKiem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kqtim_kiem);
+    }
+}
